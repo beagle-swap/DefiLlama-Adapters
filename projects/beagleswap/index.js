034c1509e77f5d726dcc8bedd333fc96aabdb442
Aiden <101932619+aiden668@users.noreply.github.com>
2023-08-25 03:20:53 +0000

    BeagleSwap V3 integrate with Defillama

---
+++ projects/beagleswap/index.js
@@ -0,0 +1,5 @@
+const { uniV3Export } = require("../helper/uniswapV3");
+
+module.exports = uniV3Export({
+  base: { factory: "0xD13aAF098d829aA25Eb69cf329a60CeF74f2d3Bf", fromBlock: 3036166, },
+});
